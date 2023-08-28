cfcf59ecee1e996d18b2442c9d5aade17a7cb196
channee math <chamon562@gmail.com>
2023-08-28 19:31:51 -0400

    add 3rd practice of selection sort

--- selectionSort.js
+++ selectionSort.js
@@ -36,7 +36,7 @@ function selectionStart(arr) {
   }
 }
 
-selectionStart(array);
+// selectionStart(array);
 
 function selectSort(array) {
   // outter loop
@@ -61,3 +61,44 @@ function selectSort(array) {
   }
   return array;
 }
+
+const array2 = [64, 34, 25, 12, 22, 11, 90];
+
+function sortySelection(array) {
+  // first have outter loop that goes to array.length -1
+  for (let i = 0; i < array.length - 1; i++) {
+    // 2nd initialize a minIndex = i
+    let minIndex = i;
+    console.log({ minIndex });
+    console.log`outter array [${array}]`;
+    for (let j = i + 1; j < array.length; j++) {
+      // 3nd have inner loop that starts at j= i+ 1 so that were to get the next number to be used to compare
+      console.log(`if array[j]: ${array[j]} < array[minIndex]: ${array[minIndex]}`)
+      console.log(`inner loop array moment [${array}]`)
+      if (array[j] < array[minIndex]) {
+        minIndex = j; // 1
+        console.log(`inner loop minIndex: ${minIndex}`);
+        // 4th inside inner loop condition that if (array[j] < array[minIndex]) assume that array[j] will be index 1 and value 34, because j = i:0 +1, j =1 and array[minIndex], is array[0] and the value is 64 because we have minIndex from the outter loop set to i, and the first iteration i would be 0. so it would look like if(34 < 64) then change the minIndex = to j and would be 1 so the index of the outer loop is set to 1 now
+      }
+      // swap logic
+    }
+    if (minIndex !== i) {
+      let temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
+  }
+  console.log(array);
+
+  // lets do a run back after the first iteration that minIndex when from 0 to now 1 because of the condition that is true 34 < 64
+
+  // after that we get out of the inner loop and into the outer loop and have a condition to swap
+
+  // if(minIndex !== i) if(1 !== 0) then swap
+  // give a temp variable to hold the array[i] temp = 64
+  // then array[i] = array[minIndex] so array[i]  is now 34
+  // and then the same array[minIndex] = equal to the temp 64
+  // array now is [34, 64, 25, 12, 22, 11, 90];
+}
+
+sortySelection(array2);
